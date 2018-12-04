7ebfad9f9660cd8737a2bbd6950f1029b61eb1d2
Aneel Marshall <aneel.marshall@outlook.com>
2018-12-04 23:23:40 +0000

    feat: add Anagram#read_file to read input, strip whitespace and return array of words

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,4 +2,15 @@ class Anagram
   def is_anagram?(word_one, word_two)
     word_one.split('').sort() == word_two.split('').sort()
   end
+
+  def read_file(file_name)
+    words = File.readlines(file_name)
+    remove_whitespace(words)
+  end
+
+  private 
+
+  def remove_whitespace(words)
+    words.map { |word| word.strip }
+  end
 end
