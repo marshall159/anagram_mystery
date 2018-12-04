ea8bd0c8417c5ebdea8654f9c5aa9c786912c448
Aneel Marshall <aneel.marshall@outlook.com>
2018-12-04 22:38:22 +0000

    test: add unit test for Anagram#is_anagram?

---
+++ spec/unit/anagram_spec.rb
@@ -0,0 +1,16 @@
+
+describe Anagram do
+  subject(:anagram) { described_class.new }
+
+  describe '#is_anagram?' do
+    it 'checks if two words are anagrams' do
+      word_one = 'silent'
+      word_two = 'listen'
+
+      are_anagrams = anagram.is_anagram?(word_one, word_two)
+
+      expect(are_anagrams).to eq(true)
+    end
+  end
+
+end
