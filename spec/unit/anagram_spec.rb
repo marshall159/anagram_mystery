08a57ad164dab500f742ff8cbabf683765a992e9
Aneel Marshall <aneel.marshall@outlook.com>
2018-12-04 22:50:53 +0000

    test: unit test if two words are not anagrams

--- spec/unit/anagram_spec.rb
+++ spec/unit/anagram_spec.rb
@@ -4,13 +4,22 @@ describe Anagram do
   subject(:anagram) { described_class.new }
 
   describe '#is_anagram?' do
-    it 'checks if two words are anagrams' do
+    it 'returns true if two words are anagrams' do
       word_one = 'silent'
       word_two = 'listen'
 
       are_anagrams = anagram.is_anagram?(word_one, word_two)
 
       expect(are_anagrams).to eq(true)
+    end
+
+    it 'returns false if two words are not anagrams' do
+      word_one = 'silent'
+      word_two = 'strike'
+
+      are_anagrams = anagram.is_anagram?(word_one, word_two)
+
+      expect(are_anagrams).to eq(false)
     end
   end
 
